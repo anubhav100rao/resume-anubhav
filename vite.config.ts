956226f8e0102b80bee8ac6125fbf3145d45afbf
Anubhav Kumar Rao <anubhav100rao@Anubhavs-MacBook-Air.local>
2025-11-01 17:45:59 +0530

    critical fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,32 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
-});
+
+
+export default defineConfig({
+  base: '/resume-anubhav/', // << set this for GitHub Pages project site
+  plugins: [react()],
+  build: {
+    outDir: 'dist' // default; you can change to 'docs' if you want to serve from /docs
+  }
+})
+
+// export default defineConfig(({ mode }) => {
+//     const env = loadEnv(mode, '.', '');
+//     return {
+//       server: {
+//         port: 3000,
+//         host: '0.0.0.0',
+//       },
+//       plugins: [react()],
+//       define: {
+//         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+//         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+//       },
+//       resolve: {
+//         alias: {
+//           '@': path.resolve(__dirname, '.'),
+//         }
+//       }
+//     };
+// });
